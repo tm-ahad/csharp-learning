0f1a9a3f1201707df4c982956dd999c0afd77be2
tm-ahad <tm.ahad.07@gmail.com>
2023-06-29 14:16:40 +0600

    FizzBuzz problem solved.

---
+++ 1-HelloWorld/FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class FizzBuzz
+{
+    static void solution(int start, int end)
+    {
+        for (int i = start; i < end; i++)
+        {
+            string res = string.Empty;
+
+            if (i % 3 == 0)
+            {
+                res += "Fizz";
+            }
+            if (i % 5 == 0)
+            {
+                res += "Buzz";
+            }
+
+            res = res == string.Empty ? "Empty" : res;
+
+            Console.WriteLine(res);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        solution(1, 100);
+    }
+}
